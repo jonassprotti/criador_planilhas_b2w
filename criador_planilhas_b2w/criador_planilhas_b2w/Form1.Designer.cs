5608ab511c4b60224bf02031955f8a930ddad05c
jonas sprocatti <jass-1999@hotmail.com>
2019-05-10 10:46:27 -0300

    Commit - 11 Programa quase concluído. Adicionado inserção de dados em sequência. Bug: Programa não reconhece o dado se a primeira inserção tiver 4 querys iniciais.

--- criador_planilhas_b2w/criador_planilhas_b2w/Form1.Designer.cs
+++ criador_planilhas_b2w/criador_planilhas_b2w/Form1.Designer.cs
@@ -28,21 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnProx = new System.Windows.Forms.Button();
+            this.txtNF = new System.Windows.Forms.TextBox();
+            this.txtNomeClie = new System.Windows.Forms.TextBox();
+            this.txtCodProd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCusto = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // BtnProx
+            // 
+            this.BtnProx.Location = new System.Drawing.Point(435, 413);
+            this.BtnProx.Name = "BtnProx";
+            this.BtnProx.Size = new System.Drawing.Size(75, 23);
+            this.BtnProx.TabIndex = 0;
+            this.BtnProx.Text = "Próximo";
+            this.BtnProx.UseVisualStyleBackColor = true;
+            this.BtnProx.Click += new System.EventHandler(this.BtnProx_Click);
+            // 
+            // txtNF
+            // 
+            this.txtNF.Location = new System.Drawing.Point(175, 209);
+            this.txtNF.Name = "txtNF";
+            this.txtNF.Size = new System.Drawing.Size(100, 20);
+            this.txtNF.TabIndex = 1;
+            // 
+            // txtNomeClie
+            // 
+            this.txtNomeClie.Location = new System.Drawing.Point(422, 209);
+            this.txtNomeClie.Name = "txtNomeClie";
+            this.txtNomeClie.Size = new System.Drawing.Size(100, 20);
+            this.txtNomeClie.TabIndex = 2;
+            // 
+            // txtCodProd
+            // 
+            this.txtCodProd.Location = new System.Drawing.Point(631, 209);
+            this.txtCodProd.Name = "txtCodProd";
+            this.txtCodProd.Size = new System.Drawing.Size(100, 20);
+            this.txtCodProd.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(429, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nome do cliente:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(628, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Código do produto:";
+            // 
+            // txtCusto
+            // 
+            this.txtCusto.Location = new System.Drawing.Point(422, 292);
+            this.txtCusto.Name = "txtCusto";
+            this.txtCusto.Size = new System.Drawing.Size(100, 20);
+            this.txtCusto.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(429, 276);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Preço de custo:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 193);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Nota fiscal:";
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Location = new System.Drawing.Point(454, 76);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(35, 13);
+            this.lblDesc.TabIndex = 10;
+            this.lblDesc.Text = "label5";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(750, 380);
+            this.ClientSize = new System.Drawing.Size(976, 448);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCusto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCodProd);
+            this.Controls.Add(this.txtNomeClie);
+            this.Controls.Add(this.txtNF);
+            this.Controls.Add(this.BtnProx);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnProx;
+        private System.Windows.Forms.TextBox txtNF;
+        private System.Windows.Forms.TextBox txtNomeClie;
+        private System.Windows.Forms.TextBox txtCodProd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCusto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDesc;
     }
 }
 
--- criador_planilhas_b2w/criador_planilhas_b2w/Form1.cs
+++ criador_planilhas_b2w/criador_planilhas_b2w/Form1.cs
@@ -18,6 +18,8 @@ namespace criador_planilhas_b2w
         private MySqlCommand objCmd = new MySqlCommand();//Comandos de linguagem
         private MySqlDataReader objDados; //Leitura de dados
 
+        String ref_pedidoInicial = "";
+
         String marca = "";
         String data_pedido = "";
         String data_estorno = "";
@@ -31,8 +33,10 @@ namespace criador_planilhas_b2w
         double valor3 = 0;
         double valor4 = 0;
         int valorInt = 0;
-
+        int add = 0;
         int j = 0;
+        private string path;
+
         public Form1()
         {
             InitializeComponent();
@@ -47,47 +51,110 @@ namespace criador_planilhas_b2w
             text = text.Replace("ACOM", ";ACOM");
             text = text.Replace("SUBA", ";SUBA");
             text = text.Replace("SHOP", ";SHOP");
+
             String[] lista = text.Split(new Char[] { ';' });
 
-            string path = @"C:\Users\desktop\Documents\ProgPlanilhas\testeResult.txt";
+           
+            for (int i = 0; i < lista.Length; i++)
+            {
+                string linha = lista[i];
+                linha = linha.Replace(";", "");
 
+                if (linha.StartsWith("01-") || linha.StartsWith("02-") || linha.StartsWith("03-"))
+                {
+                    ref_pedidoInicial = linha;
+                    break;
+                }
+            }
+            lblDesc.Text = ref_pedidoInicial;
             // This text is added only once to the file.
+
+        }
+
+        private void atualizar()
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\desktop\Documents\ProgPlanilhas\teste.txt");
+
+            text = text.Replace("ACOM", ";ACOM");
+            text = text.Replace("SUBA", ";SUBA");
+            text = text.Replace("SHOP", ";SHOP");
+
+            String[] lista = text.Split(new Char[] { ';' });
+
+            path = @"C:\Users\desktop\Documents\ProgPlanilhas\testeResult.txt";
+            if (!File.Exists(path))
+            {
+                //add++;
+                for (int i = add; i < lista.Length; i++)
+                {
+                    string linha = lista[i];
+
+                    if (linha.StartsWith("01-") && ref_pedidoInicial == "" || linha.StartsWith("02-") && ref_pedidoInicial == "" || linha.StartsWith("03-") && ref_pedidoInicial == "")
+                    {
+                        ref_pedidoInicial = linha;
+                    }
+                }
+            }
+            lblDesc.Text = ref_pedidoInicial;
+
+        }
+
+        private void adicionar()
+        {
+            
+
+            string text = System.IO.File.ReadAllText(@"C:\Users\desktop\Documents\ProgPlanilhas\teste.txt");
+
+            text = text.Replace("ACOM", ";ACOM");
+            text = text.Replace("SUBA", ";SUBA");
+            text = text.Replace("SHOP", ";SHOP");
+
+            String[] lista = text.Split(new Char[] { ';' });
+
+            path = @"C:\Users\desktop\Documents\ProgPlanilhas\testeResult.txt";
             if (!File.Exists(path))
             {
                 // Create a file to write to.
-                for (int i = 0; i < lista.Length; i++)
+                for (int i = add; i < lista.Length; i++)
                 {
+                    
                     string linha = lista[i];
                     linha = linha.Replace(";", "");
 
                     if (linha.Trim() == "")
                     {
                         data_estorno = "";
+                        add++;
                         continue;
                     }
                     if (linha.StartsWith("ACOM") || linha.StartsWith("SUBA") || linha.StartsWith("SHOP"))
                     {
                         marca = linha;
+                        add++;
                         continue;
                     }
                     if (linha.Contains("/") && data_pedido == "")
                     {
                         data_pedido = linha;
+                        add++;
                         continue;
                     }
                     if (linha.Contains("/"))
                     {
                         data_estorno = linha;
+                        add++;
                         continue;
                     }
                     if (linha.StartsWith("01-") || linha.StartsWith("02-") || linha.StartsWith("03-"))
                     {
                         ref_pedido = linha;
+                        add++;
                         continue;
                     }
                     if (linha.StartsWith("107") || linha.StartsWith("269") || linha.StartsWith("350") || linha.StartsWith("268"))
                     {
                         entrega = linha;
+                        add++;
                         continue;
                     }
                     if (linha.StartsWith("Estorno") || linha.StartsWith("Venda") || linha.StartsWith("Comissao") ||
@@ -95,6 +162,7 @@ namespace criador_planilhas_b2w
                         linha.StartsWith("Comissao") || linha.StartsWith("IR") || linha.StartsWith("Tarifa"))
                     {
                         tipo = linha;
+                        add++;
                         continue;
                     }
 
@@ -103,6 +171,7 @@ namespace criador_planilhas_b2w
                     {
                         valor = linha;
                         valorInt++;
+                        add++;
                         if (valorInt == 1)
                         {
                             valor = valor.Replace("R$", "");
@@ -136,8 +205,60 @@ namespace criador_planilhas_b2w
                             valor4 = Convert.ToDouble(valor);
                         }
                     }
+                    
                     if (valorInt == 4 && tipo != "Tarifa_Adicional")
                     {
+                        //Realiza o calculo e insere o valor da soma
+                        double receber = valor1 + valor2 + valor3;
+                        String valReceber = "INSERT INTO TESTE VALUES('', '','','','','','','','Receber','" + receber + "')";
+                        objCmd.Connection = objCnx;
+                        objCmd.CommandText = valReceber;
+                        objCnx.Open();
+                        objCmd.ExecuteNonQuery();
+                        objCnx.Close();
+                        //Insere o imposto de 4% perante o valor a receber
+                        double imposto = receber * 0.04;
+                        String valorImposto = "INSERT INTO TESTE VALUES('', '','','','','','','','Imposto NF (4%)','" + imposto + "')";
+                        objCmd.Connection = objCnx;
+                        objCmd.CommandText = valorImposto;
+                        objCnx.Open();
+                        objCmd.ExecuteNonQuery();
+                        objCnx.Close();
+                        //Insere o valor a receber tirando a taxa da NF
+                        double sNF = receber - imposto;
+                        String valorSNF = "INSERT INTO TESTE VALUES('', '','','','','','','','Receber (-NF)','" + sNF + "')";
+                        objCmd.Connection = objCnx;
+                        objCmd.CommandText = valorSNF;
+                        objCnx.Open();
+                        objCmd.ExecuteNonQuery();
+                        objCnx.Close();
+                        //Insere o preço de custo
+                        String precoCusto = "INSERT INTO TESTE VALUES('', '','','','','','','','Preço de custo','" + txtCusto.Text + "')";
+                        objCmd.Connection = objCnx;
+                        objCmd.CommandText = precoCusto;
+                        objCnx.Open();
+                        objCmd.ExecuteNonQuery();
+                        objCnx.Close();
+                        //Insere o valor final em R$
+                        double final = sNF - Convert.ToDouble(txtCusto.Text);
+                        String valorFinal = "INSERT INTO TESTE VALUES('', '','','','','','','','Lucro R$','" + final + "')";
+                        objCmd.Connection = objCnx;
+                        objCmd.CommandText = valorFinal;
+                        objCnx.Open();
+                        objCmd.ExecuteNonQuery();
+                        objCnx.Close();
+
+                        //Insere o lucro em %
+                        double finalPorc = (sNF / Convert.ToDouble(txtCusto.Text)) - 1;
+                        String valorFinalPorc = "INSERT INTO TESTE VALUES('', '','','','','','','','Lucro (%)','" + finalPorc + "%')";
+                        objCmd.Connection = objCnx;
+                        objCmd.CommandText = valorFinalPorc;
+                        objCnx.Open();
+                        objCmd.ExecuteNonQuery();
+                        objCnx.Close();
+
+
+                        //Insere uma linha vázia
                         String sqlnull = "INSERT INTO TESTE VALUES('','','','','','','','','','')";
                         objCmd.Connection = objCnx;
                         objCmd.CommandText = sqlnull;
@@ -145,19 +266,20 @@ namespace criador_planilhas_b2w
                         objCmd.ExecuteNonQuery();
                         objCnx.Close();
 
-                        String sql = "INSERT INTO TESTE VALUES('" + marca + "', '" + data_pedido + "','" + data_estorno + "',''," +
-                               "'','','" + ref_pedido + "','" + entrega + "','" + tipo + "','" + valor + "')";
-                        objCmd.Connection = objCnx;
-                        objCmd.CommandText = sql;
-                        objCnx.Open();
-                        objCmd.ExecuteNonQuery();
-                        objCnx.Close();
+                        //String sql = "INSERT INTO TESTE VALUES('" + marca + "', '" + data_pedido + "','" + data_estorno + "',''," +
+                        //       "'','','" + ref_pedido + "','" + entrega + "','" + tipo + "','" + valor + "')";
+                        //objCmd.Connection = objCnx;
+                        //objCmd.CommandText = sql;
+                        //objCnx.Open();
+                        //objCmd.ExecuteNonQuery();
+                        //objCnx.Close();
 
 
                         data_pedido = "";
                         j++;
-                        valorInt = 1;
-                        continue;
+                        valorInt = 0;
+
+                        return;
                     }
                     if (valorInt == 4)
                     {
@@ -170,7 +292,54 @@ namespace criador_planilhas_b2w
                             objCnx.Open();
                             objCmd.ExecuteNonQuery();
                             objCnx.Close();
+                            //Realiza o calculo e insere o valor da soma
+                            double receber = valor1 + valor2 + valor3 + valor4;
+                            String valReceber = "INSERT INTO TESTE VALUES('', '','','','','','','','Receber','" + receber + "')";
+                            objCmd.Connection = objCnx;
+                            objCmd.CommandText = valReceber;
+                            objCnx.Open();
+                            objCmd.ExecuteNonQuery();
+                            objCnx.Close();
+                            //Insere o imposto de 4% perante o valor a receber
+                            double imposto = receber * 0.04;
+                            String valorImposto = "INSERT INTO TESTE VALUES('', '','','','','','','','Imposto NF (4%)','" + imposto + "')";
+                            objCmd.Connection = objCnx;
+                            objCmd.CommandText = valorImposto;
+                            objCnx.Open();
+                            objCmd.ExecuteNonQuery();
+                            objCnx.Close();
+                            //Insere o valor a receber tirando a taxa da NF
+                            double sNF = receber - imposto;
+                            String valorSNF = "INSERT INTO TESTE VALUES('', '','','','','','','','Receber (-NF)','" + sNF + "')";
+                            objCmd.Connection = objCnx;
+                            objCmd.CommandText = valorSNF;
+                            objCnx.Open();
+                            objCmd.ExecuteNonQuery();
+                            objCnx.Close();
+                            //Insere o preço de custo
+                            String precoCusto = "INSERT INTO TESTE VALUES('', '','','','','','','','Preço de custo','" + txtCusto.Text + "')";
+                            objCmd.Connection = objCnx;
+                            objCmd.CommandText = precoCusto;
+                            objCnx.Open();
+                            objCmd.ExecuteNonQuery();
+                            objCnx.Close();
+                            //Insere o valor final em R$
+                            double final = sNF - Convert.ToDouble(txtCusto.Text);
+                            String valorFinal = "INSERT INTO TESTE VALUES('', '','','','','','','','Lucro R$','" + final + "')";
+                            objCmd.Connection = objCnx;
+                            objCmd.CommandText = valorFinal;
+                            objCnx.Open();
+                            objCmd.ExecuteNonQuery();
+                            objCnx.Close();
 
+                            //Insere o lucro em %
+                            double finalPorc = (sNF / Convert.ToDouble(txtCusto.Text))-1;
+                            String valorFinalPorc = "INSERT INTO TESTE VALUES('', '','','','','','','','Lucro (%)','" + finalPorc + "%')";
+                            objCmd.Connection = objCnx;
+                            objCmd.CommandText = valorFinalPorc;
+                            objCnx.Open();
+                            objCmd.ExecuteNonQuery();
+                            objCnx.Close();
 
                             String sqlnull = "INSERT INTO TESTE VALUES('','','','','','','','','','')";
                             objCmd.Connection = objCnx;
@@ -183,14 +352,39 @@ namespace criador_planilhas_b2w
                             data_pedido = "";
                             j++;
                             valorInt = 0;
-                            continue;
+
+
+                            return;
+
                         }
                         catch (Exception Erro)
                         {
                             MessageBox.Show("Erro ==> " + Erro.Message, "ADO.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
                         }
                     }
-                    if (valorInt == 1 || valorInt == 2) {
+                    if (valorInt == 1)
+                    {
+                        try
+                        {
+
+                            String sql = "INSERT INTO TESTE VALUES('" + marca + "', '" + data_pedido + "','" + data_estorno + "','" + txtNF.Text + "','" + txtNomeClie.Text + "','" + txtCodProd.Text + "','" + ref_pedido + "','" + entrega + "','" + tipo + "','" + valor + "')";
+                            objCmd.Connection = objCnx;
+                            objCmd.CommandText = sql;
+                            objCnx.Open();
+                            objCmd.ExecuteNonQuery();
+                            objCnx.Close();
+                            data_pedido = "";
+                            j++;
+                            continue;
+                        }
+
+                        catch (Exception Erro)
+                        {
+                            MessageBox.Show("Erro ==> " + Erro.Message, "ADO.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else if (valorInt == 2)
+                    {
                         try
                         {
                             String sql = "INSERT INTO TESTE VALUES('" + marca + "', '" + data_pedido + "','" + data_estorno + "',''," +
@@ -204,12 +398,13 @@ namespace criador_planilhas_b2w
                             j++;
                             continue;
                         }
-                        
-                    catch (Exception Erro)
-                    {
-                        MessageBox.Show("Erro ==> " + Erro.Message, "ADO.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        catch (Exception Erro)
+                        {
+                            MessageBox.Show("Erro ==> " + Erro.Message, "ADO.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
                     }
-                  } else if(valorInt == 3)
+                    else if (valorInt == 3)
                     {
                         try
                         {
@@ -223,8 +418,18 @@ namespace criador_planilhas_b2w
                             data_pedido = "";
                             j++;
 
+                            
 
-                            continue;
+                            if (tipo != "Frete_B2W_Entrega" || tipo != "Estorno_Frete_B2W_Entrega")
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                               
+                                return;
+                            }
+                            
                         }
 
                         catch (Exception Erro)
@@ -233,13 +438,23 @@ namespace criador_planilhas_b2w
                         }
                     }
                     
-                }
-                File.WriteAllLines(path, lista, Encoding.UTF8);    
-            }
 
-            atualizar next = new atualizar();
-            next.Show();
-            this.Visible = false;
+                }
+
+                File.WriteAllLines(path, lista, Encoding.UTF8);
+            }
+        }
+
+        private void BtnProx_Click(object sender, EventArgs e)
+        {
+
+            adicionar();
+            ref_pedidoInicial = "";
+            atualizar();
+            valor1 = 0;
+            valor2 = 0;
+            valor3 = 0;
+            valor4 = 0;
         }
     }
 }
